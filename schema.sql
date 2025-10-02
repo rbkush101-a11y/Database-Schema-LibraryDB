3630a06308fd284bd0041a9cb1cfc8a66e96ac8b
rbkush101-ally <rbkush101@gmail.com>
2025-10-02 19:00:27 +0530

    Modify code

--- schema.sql
+++ schema.sql
@@ -1,9 +1,6 @@
---- Create Database
+use library;
 
-create database librarydb;
-use librarydb;
-
---- Books Table
+-- Books Table
 
 create table books (
     book_id int auto_increment primary key,
@@ -14,7 +11,7 @@ create table books (
     available_copies int default 0
 );
 
---- Members Table
+-- Members Table
 
 create table members (
     member_id int auto_increment primary key,
@@ -22,16 +19,16 @@ create table members (
     last_name varchar(100) not null,
     email varchar(255) unique not null,
     phone varchar(15),
-    join_date date default current_date
+    join_date date default (current_date)
 );
 
---- Borrowings Table
+-- Borrowings Table
 
 create table borrowings (
     borrowing_id int auto_increment primary key,
     book_id int,
     member_id int,
-    borrow_date date default current_date,
+    borrow_date date default (current_date),
     return_date date,
     due_date date,
     foreign key (book_id) references books(book_id),
